ee6d2ee108011298ccb59eacf22bd96f807420f5
liuzhe <zhethink@gmail.com>
2019-12-16 00:31:00 +0800

    添加懒加载

--- src/index.js
+++ src/index.js
@@ -14,3 +14,16 @@ img.src = png1
 
 
 div.appendChild(img)
+
+let button = document.createElement('button');
+button.innerText = '懒加载';
+
+div.appendChild(button)
+button.onclick = ()=>{
+  let promise = import('./lazy.js')
+  promise.then((module)=>{
+    console.log('斤斤计较');
+    let fn = module.default;
+    fn()
+  })
+}
