9fb9f3f43b633fe8658a839d5182e70ca970a310
liuzhe <zhethink@gmail.com>
2019-12-15 23:01:01 +0800

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 var path = require('path');
 
 module.exports = {
@@ -6,5 +7,11 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js'
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'lttztt - web',
+      template: 'src/assets/index.html'
+    })
+  ]
 };
