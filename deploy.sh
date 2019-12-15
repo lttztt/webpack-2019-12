711d02a2c352f3c520b913956754801d6e0d8e2e
liuzhe <zhethink@gmail.com>
2019-12-16 00:56:36 +0800

    fix deploy bug2

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 yarn build &&
 git checkout gh-pages && 
 rm *.html *.js *.css *.png *.jpeg *.jpg *.gif && 
-mv dist/* ./ ;
-rm -rf dist && 
+mv dist/* ./ &&
+rm -rf dist;
 git add . &&
 git commit -m 'update' &&
 git push && 
