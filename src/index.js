c765d4a9e934d12f62b1b2fb22a90155c5a9dfee
liuzhe <zhethink@gmail.com>
2019-12-16 00:21:24 +0800

    add file loader to load img

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import xx from './x.js'
 import './style.scss'
 // console.log(xx);
 console.log('hello');
+
+import png1 from './img/1.png'
+
+let div = document.getElementById('app')
+
+console.log(div);
+let img = document.createElement('img')
+img.src = png1
+
+
+div.appendChild(img)
